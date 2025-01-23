21db506515c1937e96223ad217c37186970f82ca
dyfldrpf9 <dyfldrpf9@naver.com>
2025-01-23 20:50:38 +0900

    home button 추가 및 JavascriptMode 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -10,7 +10,9 @@ class HomeScreen extends StatelessWidget {
   com.android.application 버전을 기본(8.0.1)에서 최신(8.2.2)로
   수정해야 실행할 수 있음.
   */
-  WebViewController controller = WebViewController()..loadRequest(homeUrl);
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(homeUrl);
 
   HomeScreen({super.key});
 
@@ -22,6 +24,13 @@ class HomeScreen extends StatelessWidget {
         title: Text('WebView Practice',
             style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700)),
         centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                controller.loadRequest(homeUrl);
+              },
+              icon: Icon(Icons.home))
+        ],
       ),
       body: WebViewWidget(controller: controller),
     );
